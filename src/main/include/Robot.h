537a03e81d73a25f978cef5233e58547a485cbf6
Harsh-Sinha <harsh.sinha@hotmail.com>
2023-12-10 11:21:15 -0500

    test: get rid of unecessary space

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -31,7 +31,4 @@ class Robot : public frc::TimedRobot {
   const std::string kAutoNameDefault = "Default";
   const std::string kAutoNameCustom = "My Auto";
   std::string m_autoSelected;
-
-  
-
 };
