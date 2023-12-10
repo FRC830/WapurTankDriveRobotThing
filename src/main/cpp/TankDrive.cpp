9df25aa8fc8a0edf9350a3b71650153907a05fae
RED2 <RED2@ratpack>
2023-12-10 17:04:59 -0500

    all the dead zone code you forgot to push

--- src/main/cpp/TankDrive.cpp
+++ src/main/cpp/TankDrive.cpp
@@ -1,7 +1,26 @@
 #include "TankDrive.h"
 
 void TankDrive::Drive(){
-  d.ArcadeDrive(pilot.GetLeftY()/2, pilot.GetRightX()/2, true);
+  int deadzone = 0.3;
+  // d.ArcadeDrive(pilot.GetLeftY()/2, pilot.GetRightX()/2, true);
+  if (pilot.GetLeftY() > deadzone) {
+    if (pilot.GetLeftBumper()){
+        d.ArcadeDrive(((pilot.GetLeftY()) + deadzone) / (1 - deadzone)/2,  pilot.GetRightX()/2, false);
+    }
+    
+    else{
+      d.ArcadeDrive(((pilot.GetLeftY()) + deadzone) / (1 - deadzone), pilot.GetRightX()/2, false);
+    }
+  }
+  else if(pilot.GetLeftY() < -deadzone) {
+    if (pilot.GetLeftBumper()){
+        d.ArcadeDrive(((pilot.GetLeftY()) - deadzone) / (1 - deadzone)/2,  pilot.GetRightX()/2, false);
+    }
+    
+    else{
+      d.ArcadeDrive(((pilot.GetLeftY()) - deadzone) / (1 - deadzone), pilot.GetRightX()/2, false);
+    }
+  }
+  }
   // mL.Set(pilot.GetLeftX());
   // mR.Set(pilot.GetRightX());
-}
